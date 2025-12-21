f8039397246fd1de2e04b27059cb73efa609f033
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2025-12-21 17:58:36 +0200

    Add top and bottom barriers

--- Scenes/Tappy/Tappy.gd
+++ Scenes/Tappy/Tappy.gd
@@ -3,18 +3,38 @@ extends CharacterBody2D
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 var _jumped: bool = false
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 const JUMP_POWER: float = -350.0
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("power"):
 		_jumped = true
 
+func die() -> void:
+	set_physics_process(false)
+	animated_sprite_2d.stop()
+
 func _ready() -> void:
 	pass # Replace with function body.
+
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("power"):
+		#_jumped = true
 
 func _physics_process(delta: float) -> void:
 	velocity.y += _gravity * delta
 	if _jumped:
 		velocity.y = JUMP_POWER
 		_jumped = false
+		
+	#if Input.is_action_just_pressed("power"): # An option but is checked for every phtsics frame and unhandled will just signal when it's pressed
+		#velocity.y = JUMP_POWER
+		
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
+	
+	if is_on_ceiling():
+		print("on ceiling")
