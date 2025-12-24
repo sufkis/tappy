eac3642f9f019b2871c15a8ec8ae27a100966ef6
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2025-12-25 01:36:12 +0200

    Add a parallax background

--- Scenes/Pipes/Pipes.gd
+++ Scenes/Pipes/Pipes.gd
@@ -4,6 +4,8 @@ class_name Pipes
 
 const PIPE_SPEED: float = 120.0
 
+@onready var score_sound: AudioStreamPlayer2D = $ScoreSound
+
 func _ready() -> void:
 	pass
 
@@ -23,4 +25,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_laser_body_exited(body: Node2D) -> void:
 	if body is Tappy:
+		score_sound.play()
 		SignalHub.emit_on_point_scored()
